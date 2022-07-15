7050b16bec3009f2911aa6fac665d122cb20a50f
Elvis Chege <elvischege@icloud.com>
2022-07-15 06:53:28 +0200

    Code refactor

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import os
+from fastapi import HTTPException
 from jose import JWTError, jwt
+from fastapi import status
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 3000  # 30 minutes
 REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 # 7 days
@@ -8,7 +10,7 @@ ALGORITHM = "HS256"
 JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']     # should be kept secret
 JWT_REFRESH_SECRET_KEY = os.environ['JWT_REFRESH_SECRET_KEY']      # should be kept secret
 
-async def get_current_user(token: str = Depends(oauth2_scheme),  db: Session = Depends(get_db)):
+async def get_current_user(token: str):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -19,12 +21,8 @@ async def get_current_user(token: str = Depends(oauth2_scheme),  db: Session = D
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-        token_data = TokenData(username=username)
+        return username
     except JWTError:
         raise credentials_exception
-
-    # user = get_user(fake_users_db, username=token_data.username)
-    user = db.query(UserModel).filter(UserModel.email ==token_data.username).first()
-    if user is None:
-        raise credentials_exception
-    return user
+        
+    
